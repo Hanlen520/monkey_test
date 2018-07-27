0ad9204ae19e52777c4c720d579c0bf693361539
samesky2016 <84028058>
2018-07-27 10:06:41 +0800

    增加配置文件方式读取配置。

--- Base/Config.py
+++ Base/Config.py
@@ -1,19 +1,24 @@
 # /usr/bin/env python
 # -*- encoding: utf-8 -*-
 import random
-
+import configparser
 class Config:
+
+    conf = configparser.ConfigParser()
+    conf.read("../config//config.ini","utf-8")
     # apk包名
-    package_name = "com.jieshun.jslife"
+    package_name = conf.get("monkey","package_name")
     # 默认设备列表
     device_dict = {}
     # 网络
-    net = "wifi"
+    net = conf.get("monkey","net")
     # monkey seed值，随机产生
     monkey_seed = str(random.randrange(1, 1000))
     # monkey 参数
-    monkey_parameters = "--throttle 300 --ignore-crashes --ignore-timeouts --pct-touch 60 --pct-trackball 12 --pct-appswitch 5 --pct-syskeys 10 --pct-motion 13 -v -v 2000"
+    monkey_parameters = conf.get("monkey","monkey_parameters")
     # log保存地址
-    log_location = "D:\\monkey_test\\log\\"
+    log_location = conf.get("monkey","log_location")
     #性能数据存储目录
-    info_path = "D:\\monkey_test\\info\\"
+    info_path = conf.get("monkey","info_path")
+
+    print(info_path)
